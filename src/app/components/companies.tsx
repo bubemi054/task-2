9f22ac1344e85c8149137328db0412a4306f445b
deji100 <adeyemideji9@gmail.com>
2025-04-02 10:49:54 +0100

    updated

--- src/app/components/companies.tsx
+++ src/app/components/companies.tsx
@@ -2,12 +2,29 @@
 
 import { useEffect, useState } from "react";
 import { useUserSessionChecker } from "../hooks/useUserSessionChecker";
-// import { Company } from "../types";
 import NavBar from "./Navbar";
+import { useCompanies } from "../hooks/useCompanies";
+import Image from "next/image";
+import { useApolloClient } from "@apollo/client";
+import imgSample from "../../../public/next.svg"
 
 const CompanyCard = ({ company }: { company: Company }) => {
+  // const client = useApolloClient();
+  // const { getFileImage } = useCompanies({client});
+  // const [url, setUrl] = useState("");
+
+  // useEffect(() => {
+  //   const getImgUrl = async () => {
+  //     const {url} = await getFileImage(company.logoS3Key)
+  //     setUrl(url)
+  //   }
+
+  //   getImgUrl()
+  // }, [])
+
   return (
-    <div className="w-[304px] h-[304px] bg-[#F0F0F0] rounded-[20px] p-[20px] flex flex-col justify-center items-center">
+    <div className="w-[250px] h-[250px] bg-[#F0F0F0] rounded-[20px] p-[20px] flex flex-col justify-center items-center gap-2 cursor-pointer">
+      <Image className="w-[100px] h-[70px]" src={imgSample} alt="Company Image" width={1000} height={1000} />
       <span className="font-bold">{company.legalName}</span>
       <span>{company.industry}</span>
       <span>{company.email}</span>
